43ef16b700396debb5c9edf659453150db1fdc25
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-29 14:36:37 -0300

    validations for the auth

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -20,7 +20,32 @@ const registrar = async (req, res) => {
 }
 
 const autenticar = async (req, res) => {
-    res.status(200).json({msg: "Ruta de autenticar"})
+    const {email, password} = req.body
+
+    // comprobar si el usuario existe
+    const usuario = await Usuario.findOne({email})
+    if(!usuario){
+        const error = new Error('El usuario no existe')
+        return res.status(404).json({msg: error.message})
+    }
+
+    // comprobar si el usuario esta confirmado
+    if(!usuario.confirmado){
+        const error = new Error('Tu cuenta no ha sido confirmada')
+        return res.status(403).json({msg: error.message})
+    }
+
+    // comprobar su password
+    if(await usuario.comprobarPassword(password)){
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email
+        })
+    }else{
+        const error = new Error('El password es incorrecto (email o password incorrectos)')
+        return res.status(403).json({msg: error.message})
+    }
 }
 
 export {
--- models/Usuario.js
+++ models/Usuario.js
@@ -37,6 +37,11 @@ usuarioSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password, salt)
 })
 
+// creamos la funcion "comprobarPassword"
+usuarioSchema.methods.comprobarPassword = async function(passwordFormulario){
+    return await bcrypt.compare(passwordFormulario, this.password)
+}
+
 
 const Usuario = mongoose.model("Usuario", usuarioSchema)
 export default Usuario
