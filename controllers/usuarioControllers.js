d3683cc6686328318906d572881506d3a0d6413c
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-29 11:54:31 -0300

    preventing duplicate users

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,6 +1,13 @@
 import Usuario from "../models/Usuario.js"
 
 const registrar = async (req, res) => {
+    // evitar registros duplicados
+    const {email} = req.body
+    const existeUsuario = await Usuario.findOne({email})
+    if(existeUsuario){
+        const error = new Error('Usuario ya registrado')
+        return res.status(400).json({msg: error.message}) // ".message" accede al mensaje de arriba
+    }
     try {
         const usuario = new Usuario(req.body) // crear usuario con la informacion del modelo
         const usuarioAlmacenado = await usuario.save() // almacenar en mongoDB
