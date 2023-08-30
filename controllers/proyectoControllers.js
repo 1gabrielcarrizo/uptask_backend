368afd4adf2f98f0f8561f647218bccc0b2b5a07
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-29 22:38:17 -0300

    edit a project by id

--- controllers/proyectoControllers.js
+++ controllers/proyectoControllers.js
@@ -35,7 +35,30 @@ const obtenerProyecto = async (req, res) => {
 }
 // put para editar un proyecto en especifico
 const editarProyecto = async (req, res) => {
-    
+    const {id} = req.params
+    // consultar si el proyecto existe en la DB
+    const proyecto = await Projecto.findById(id)
+    if(!proyecto){
+        const error = new Error("Proyecto no encontrado")
+        return res.status(404).json({msg: error.message})
+    }
+    // si no es el creador o un colaborador..
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+        const error = new Error("Accion no valida (no tienes los permisos)")
+        return res.status(401).json({msg: error.message})
+    }
+    // actualiza los campos por separado
+    proyecto.nombre = req.body.nombre || proyecto.nombre
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega
+    proyecto.cliente = req.body.cliente || proyecto.cliente
+
+    try {
+        const proyectoAlmacenado = await proyecto.save()
+        return res.json(proyectoAlmacenado)
+    } catch (error) {
+        console.error(error)
+    }
 }
 // delete para eliminar un proyecto en especifico
 const eliminarProyecto = async (req, res) => {
