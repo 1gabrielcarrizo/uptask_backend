faf1e73ff512bf44ade0cbc68fd8147e7b4a5202
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-08-31 18:15:25 -0300

    adding environment variables

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,3 +1,4 @@
+import { emailRegistro, emailOlvidePassword } from "../helpers/email.js"
 import generarId from "../helpers/generarId.js"
 import generarJWT from "../helpers/generarJWT.js"
 import Usuario from "../models/Usuario.js"
@@ -6,15 +7,21 @@ const registrar = async (req, res) => {
     // evitar registros duplicados
     const {email} = req.body
     const existeUsuario = await Usuario.findOne({email})
-    if(existeUsuario){
+    if(existeUsuario){ // si ya existe un usuario con ese email..
         const error = new Error('Usuario ya registrado')
         return res.status(400).json({msg: error.message}) // ".message" accede al mensaje de arriba
     }
     try {
         const usuario = new Usuario(req.body) // crear usuario con la informacion del modelo
         usuario.token = generarId() // agregamos la propieda "token" y generamos uno
-        const usuarioAlmacenado = await usuario.save() // almacenar en mongoDB
-        res.status(201).json(usuarioAlmacenado) // retornamos el usuario almacenado
+        await usuario.save() // almacenar en mongoDB
+        // enviar el email de confirmacion
+        emailRegistro({
+            email: usuario.email,
+            nombre: usuario.nombre,
+            token: usuario.token
+        })
+        return res.json({msg: 'Usuario Creado Correctamente, revisa tu email para confirmar tu cuenta'})
     } catch (error) {
         console.error(error)
     }
@@ -53,8 +60,9 @@ const autenticar = async (req, res) => {
 const confirmar = async (req, res) => {
     const {token} = req.params // escribimos "token" porque en usuarioRoutes lo definimos asi
     const usuarioConfirmar = await Usuario.findOne({token})
+
     if(!usuarioConfirmar){
-        const error = new Error('Token no valido')
+        const error = new Error('Token no válido')
         return res.status(403).json({msg: error.message})
     }
 
@@ -81,6 +89,12 @@ const olvidePassword = async (req, res) => {
         // si el usuario existe...
         usuario.token = generarId()
         await usuario.save()
+        // enviar el email en caso de olvidar el password
+        emailOlvidePassword({
+            email: usuario.email,
+            nombre: usuario.nombre,
+            token: usuario.token
+        })
         return res.json({msg: 'Hemos enviado un email con las instrucciones'})
     } catch (error) {
         console.error(error)
--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 import express from 'express' // ESM
 // import 'dotenv/config'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import conectarDB from './config/db.js'
 import usuarioRoutes from './routes/usuarioRoutes.js'
 import proyectoRoutes from './routes/proyectoRoutes.js'
@@ -17,6 +18,23 @@ dotenv.config()
 
 // conectar al mongoDB
 conectarDB()
+
+// configurar cors
+const whiteList = [process.env.FRONTEND_URL]
+const corsOptions= {
+    // quien esta viendo el request
+    origin: function(origin, callback){
+        if(whiteList.includes(origin)){
+            // puede consultar la API
+            callback(null, true)
+        }else{
+            // no tiene permitido consultar la API
+            callback(new Error('Error de Cors'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 // routing
 app.use("/api/usuarios", usuarioRoutes)
