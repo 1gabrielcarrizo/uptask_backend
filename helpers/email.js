759e75ad5e4b5fa6d0a8722fe28befff42aab04d
1gabrielcarrizo <gabriel_glee_97@hotmail.com>
2023-10-07 15:03:49 -0300

    sending emails by gmail

--- helpers/email.js
+++ helpers/email.js
@@ -3,53 +3,67 @@ import nodemailer from 'nodemailer'
 // se envia el nombre del usuario, el email y el token
 export const emailRegistro = async (datos) => {
     const { email, nombre, token } = datos
-    // copiamos el codigo de la pagina mailtrap
+    try {
+        // copiamos el codigo de la pagina mailtrap
     const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        host: process.env.GMAIL_HOST,
+        port: process.env.GMAIL_PORT,
+        service: "gmail",
+        secure: true, // seguridad
         auth: {
             // crearse una cuenta en mailtrap para que te den el user y pass
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
         }
     });
     // informacion del email
     const info = await transport.sendMail({
-        from: '"UpTask - Administrador de Proyectos <cuentas@uptask.com>"',
+        from: '"ProjectMasterAdm - Administrador de Proyectos <cuentas@uptask.com>"',
         to: email,
-        subject: "UpTask - Confirma tu cuenta",
-        text: "Comprueba tu cuenta en UpTask",
-        html: ` <p>Hola ${nombre}: Comprueba tu cuenta en UpTask</p>
+        subject: "ProjectMasterAdm - Confirma tu cuenta",
+        text: "Comprueba tu cuenta en ProjectMasterAdm",
+        html: ` <p>Hola ${nombre}:</p>
+        <p>Comprueba tu cuenta en ProjectMasterAdm.</p>
         <p>Tu cuenta ya esta casi lista, solo debes comprobarla en el siguiente enlace:
         <a href="${process.env.FRONTEND_URL}/confirmar/${token}">Comprobar Cuenta</a></p>
         <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje.</p>
         `
     })
+    } catch (error) {
+        console.error("No se pudo enviar el email para registrarse")
+    }
 }
 
-// se envia el email
+// se envia el email para crear un nuevo password
 export const emailOlvidePassword = async (datos) => {
     const { email, nombre, token } = datos
-    // copiamos el codigo de la pagina mailtrap
+    try {
+        // copiamos el codigo de la pagina mailtrap
     const transport = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
+        host: process.env.GMAIL_HOST,
+        port: process.env.GMAIL_PORT,
+        service: "gmail",
+        secure: true, // seguridad
         auth: {
             // crearse una cuenta en mailtrap para que te den el user y pass
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
+            user: process.env.GMAIL_USER,
+            pass: process.env.GMAIL_PASS
         }
     });
     // informacion del email
     const info = await transport.sendMail({
-        from: '"UpTask - Administrador de Proyectos <cuentas@uptask.com>"',
+        from: '"ProjectMasterAdm - Administrador de Proyectos <cuentas@uptask.com>"',
         to: email,
-        subject: "UpTask - Reestablece tu Password",
+        subject: "ProjectMasterAdm - Reestablece tu Password",
         text: "Reestablece tu Password",
-        html: ` <p>Hola ${nombre}: Has solicitado reestablecer tu password.</p>
+        html: ` <p>Hola ${nombre}:</p>
+        <p>Has solicitado reestablecer tu password.</p>
         <p>Sigue el siguiente enlace para generar un nuevo password:
         <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestablecer Password</a></p>
         <p>Si tu no solicitaste este email, puedes ignorar el mensaje.</p>
         `
     })
+    } catch (error) {
+        console.error("No se pudo enviar el email para crear una nueva contraseña")
+    }
 }
